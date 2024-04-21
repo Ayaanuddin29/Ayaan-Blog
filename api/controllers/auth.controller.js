831ff29d2c1ef2e5c36f648b354856dbda0c6289
Ayaanuddin <khajaayaan2912@gmail.com>
2024-04-21 14:59:57 +0530

    error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup=async(req,res)=>{
+import { errorHandler } from "../utils/error.js";
+export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     if(!username||!email||!password||username==''||email==''||password==''){
-        return res.status(400).json({message:"All Fields are required"});
+       next(errorHandler(400,'All Fields are Required'))
     }
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser=new User({username,email,password:hashedPassword});
@@ -12,7 +13,7 @@ export const signup=async(req,res)=>{
         res.json({message:"Signup Successfull"})
     }
     catch(err){
-        res.json('duplicate')
+       next(err);
     }
     
 };
