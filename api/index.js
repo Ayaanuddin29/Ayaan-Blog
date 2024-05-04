d7618575d7d796809c53a9c7e6b40f8f664e31d0
Ayaanuddin <khajaayaan2912@gmail.com>
2024-05-04 11:36:25 +0530

    create post api backend

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoute from './routes/user.route.js';
+import postRoutes from './routes/post.route.js'
 import authRoute from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 const app=express();
@@ -18,7 +19,7 @@ app.listen(3000,()=>{
 })
 app.use('/api/user/',userRoute);
 app.use('/api/auth/',authRoute);
-
+app.use('/api/post/',postRoutes);
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
