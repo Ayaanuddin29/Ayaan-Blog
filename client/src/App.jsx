bd22113a72da013c387ff10d717f2c40e1bf1402
Ayaanuddin <khajaayaan2912@gmail.com>
2024-05-07 12:48:07 +0530

    update post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from "./components/Footer"
 import PrivateRoute from "./components/PrivateRoute"
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import CreatePost from "./pages/CreatePost"
+import UpdatePost from "./pages/UpdatePost"
 export default function App() {
   return (
     <div>
@@ -24,10 +25,10 @@ export default function App() {
         <Route path='/dashboard' element={<Dashboard/>}></Route>
         </Route>
         <Route element={<OnlyAdminPrivateRoute/>}>
-        <Route path='/create-post' element={<CreatePost/>}></Route>
+        <Route path='/create-post' element={<CreatePost/>}/>
+        <Route path='/update-post/:postId' element={<UpdatePost/>}/>
         </Route>
         <Route path='/about' element={<About/>}></Route>
-        <Route></Route>
       </Routes>
       <Footer/>
      </BrowserRouter>
