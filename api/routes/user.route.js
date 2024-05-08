1baa8570c81c0957696c7880af0cf261b60878b4
Ayaanuddin <khajaayaan2912@gmail.com>
2024-05-08 15:37:21 +0530

    api route for no of users

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router=express.Router();
-import  {deleteUser, signout, test, updateUser}  from '../controllers/user.controller.js';
+import  {deleteUser, getUsers, signout, test, updateUser}  from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 router.get('/test',test);
 router.put('/update/:userId',verifyToken,updateUser);
 router.delete('/delete/:userId',verifyToken,deleteUser);
 router.post('/signout',signout);
+router.get('/getusers',verifyToken,getUsers)
 export default router;
