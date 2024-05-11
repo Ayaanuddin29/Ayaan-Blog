b5ec75a02d8f2803878a6427ee31e256073facf6
Ayaanuddin <khajaayaan2912@gmail.com>
2024-05-11 14:27:49 +0530

    comment func

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoute from './routes/user.route.js';
 import postRoutes from './routes/post.route.js'
 import authRoute from './routes/auth.route.js';
+import commentRoute from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
 const app=express();
 app.use(express.json())
@@ -20,6 +21,7 @@ app.listen(3000,()=>{
 app.use('/api/user/',userRoute);
 app.use('/api/auth/',authRoute);
 app.use('/api/post/',postRoutes);
+app.use('/api/comment/',commentRoute);
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
