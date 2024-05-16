f0183fbb7caead100e2067ea120e19e3bab36c0e
Ayaanuddin <khajaayaan2912@gmail.com>
2024-05-16 10:18:59 +0530

    update

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
              <h1 className="text-3xl font font-semibold text-center my-7">About Ayaan's Blog</h1>
              <div className="text-md text-gray-500 flex flex-col gap-6">
               <p>
-              Welcome to Ayaan's Blog! This blog was created by Sahand Ghavidel as a personal project to share his thoughts and ideas with the world.Ayaan is a passionate developer who loves to write about technology, coding, and everything in between.
+              Welcome to Ayaan's Blog! This blog was created by Ayaan as a personal project to share his thoughts and ideas with the world.Ayaan is a passionate developer who loves to write about technology, coding, and everything in between.
               </p>
               <p>
               On this blog, you'll find weekly articles and tutorials on topics such as web development, software engineering, and programming languages. Ayaan is always learning and exploring new technologies, so be sure to check back often for new content!
