901620d066fdb5b04f9e624ceddecb5332559a9d
Ayaanuddin <khajaayaan2912@gmail.com>
2024-05-16 09:56:22 +0530

    adding the script for deployment

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import postRoutes from './routes/post.route.js'
 import authRoute from './routes/auth.route.js';
 import commentRoute from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 const app=express();
 app.use(express.json())
 dotenv.config();
@@ -14,6 +16,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch(err=>{
     console.log(err)
 })
+const __dirname=path.resolve();
 app.use(cookieParser())
 app.listen(3000,()=>{
     console.log('server is running!!!!!!!!!!')
@@ -22,6 +25,15 @@ app.use('/api/user/',userRoute);
 app.use('/api/auth/',authRoute);
 app.use('/api/post/',postRoutes);
 app.use('/api/comment/',commentRoute);
+
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+});
+
+
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
