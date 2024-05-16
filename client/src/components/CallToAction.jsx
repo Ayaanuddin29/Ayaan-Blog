ffe101c1d530e7743d101282f01bc97f0225fd43
Ayaanuddin <khajaayaan2912@gmail.com>
2024-05-16 11:24:27 +0530

    updating

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -8,7 +8,7 @@ function CallToAction() {
             Want to learn more about JavaScript
         </h2>
         <p className="text-gray-500 my-2">Checkout these resources with 100 JavaScript Projects</p>
-        <Button className="rounded-tl-xl m-5  w-full rounded-bl-none" gradientDuoTone='purpleToPink'><a href='https://www.100jsprojects.com' rel='noopener noreferrer' target="_blank">100 JavaScript Projects</a>  </Button>
+        <Button className="rounded-tl-xl w-full rounded-bl-none" gradientDuoTone='purpleToPink'><a href='https://www.100jsprojects.com' rel='noopener noreferrer' target="_blank">100 JavaScript Projects</a>  </Button>
         </div>
         <div className="p-7 flex-1">
             <img src='https://1.bp.blogspot.com/-KLh-NQa0LS4/XgOK1kgdBKI/AAAAAAAADBk/MbLnp4gy_OsZx5O5S_tA7vWNBEyoIVeWQCLcBGAsYHQ/w1200-h630-p-k-no-nu/java-script.png'/>
